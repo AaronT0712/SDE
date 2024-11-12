336fd921eb02695db9fbeee1f1806778fef35aaf
Yuhan Tan <114288380+AaronT0712@users.noreply.github.com>
2024-11-12 01:27:39 -0500

    完成初始化和mybatisp的使用

--- user-center/user-center/src/main/java/com/tyh/usercenter/UserCenterApplication.java
+++ user-center/user-center/src/main/java/com/tyh/usercenter/UserCenterApplication.java
@@ -1,13 +1,13 @@
 package com.tyh.usercenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tyh.usercenter.mapper")
 public class UserCenterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
     }
-
 }
